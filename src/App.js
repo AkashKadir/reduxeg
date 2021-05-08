f0eba3dd2b690f24d095bb6556425ce8a6d22e9c
Akash <akashjaha00@gmail.com>
2021-05-08 18:57:38 +0530

    app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React sincerely
+          Learn redux
         </a>
       </header>
     </div>
